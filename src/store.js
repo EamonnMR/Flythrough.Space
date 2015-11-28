b00c01e66c46c6ad9d2b917ec27955752c1f95c5
Eamonn <eamonn.m.r@gmail.com>
2015-11-28 17:31:22 -0500

    mapView: Draw map on canvas
    Also has the skeleton of methods used
    to drag map around, linked with ng-
    methods to index.html.

--- src/store.js
+++ src/store.js
@@ -1,9 +1,50 @@
 var app = angular.module('storeApp', []);
-app.controller('storeController', function($scope) {
-   $scope.firstName = "John";
-   $scope.lastName = "Doe";
-   $scope.view_state = "menu"
-   $scope.enterState = (state) => {
-     $scope.view_state = state
-   };
+app.controller('storeController', function($scope, $http) {
+  $scope.firstName = "John";
+  $scope.lastName = "Doe";
+  $scope.view_state = "menu"
+  $scope.enterState = (state) => {
+    $scope.view_state = state;
+  };
+
+  class MapView {
+    constructor(data, location, drawContext) {
+      this.offset = location;
+      this.move = {'x': 0, 'y': 0};
+      this.map_size = {'x': 0, 'y': 0};
+      this.data = data;
+      this.context = drawContext;
+      this.dragging = false;
+    }
+
+    mouseUp($event) {
+      this.dragging = false;
+      console.log('MouseUp');
+    }
+
+    mouseDown($event) {
+      this.dragging = true;
+      console.log('mouseDown')
+    }
+
+    mouseMove($event) {
+      if (this.dragging) {
+        console.log("Dragging map")
+        this.draw();
+      }
+    }
+
+    draw(){
+      this.context.fillStyle = "Black";
+      this.context.fillRect(0, 0, 851, 600);
+    }
+  };
+  $http.get('data/systems.json')
+       .then(function(res){
+          $scope.map = new MapView(res.data,
+                               {'x': 0, 'y': 0},
+                               $('#mapcanvas')[0].getContext('2d'));
+          console.log('Downloaded the map')
+          $scope.map.draw();
+        });
 });
