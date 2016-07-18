a300f52994b31a4c2ad44daa16c07b96627bd5bc
= <=>
2016-07-18 07:48:20 -0400

    First draft of collision system

---
+++ src/collisionService.js
@@ -0,0 +1,30 @@
+angular
+.module('mainApp')
+.service('physicsService', function() {
+
+function distance(l_pos, r_pos){
+  return Math.sqrt(
+    Math.pow(l_pos.x - r_pos.x, 2)
+    + Math.pow(l_pos.y - r_pos.y, 2)
+    );
+  },
+
+return {
+
+collisionDetectionSystem: function(entMan){
+  let colliders = entMan.get_with(['collider', 'position'])
+  while (colliders.length > 1) {
+    let current = colliders.pop();
+    for (let other of colliders) {
+      if ( distance(current.position, other.position) <
+           current.collider.radius + other.collider.radius) {
+        // Do something with the collision 
+        console.log('Colliding: ' + current.id + ', ' + other.id;
+      }
+    }
+  }
+}
+
+};
+
+})
