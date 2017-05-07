05fd3433af409fde19c37a73467e88b702309e2f
EamonnMR <eamonn.m.r@gmail.com>
2017-05-07 16:49:25 -0400

    Adds line breaking to show more text
    I thought this one was going to be a huge pain, but it wasn't actually
    that difficult. It seems pretty gross to be re-implementing the most
    basic functionality of a browser in a browser, but I don't see any
    alternatives (TODO: Find a better alternative)

--- src/landing.js
+++ src/landing.js
@@ -7,16 +7,29 @@ export class LandingMainView extends menu.MainMenuView {
     this.player_data = player_data;
   }
   enter(){
+    console.log(this.player_data.current_spob);
+    console.log(this.spobs[this.player_data.current_spob]);
+    this.spob = this.spobs[this.player_data.current_spob];
+    console.log(this.spob);
     this.widgets = this.get_widgets_for_planet();
     this.setup_2d_canvas();
   }
 
   get_widgets_for_planet(){
-    return {
-      'leave_btn': new menu.TextButton('leave ' + this.player_data.current_spob,
+    let widgets = {
+      'leave_btn': new menu.TextButton(
+          'leave ' + this.player_data.current_spob,
         () => {
           this.parent.enter_state('gameplay');
         }, 0, 0, 'leave_btn'),
-    };  
+    };
+
+    if (this.spob.text){
+      console.log(this.dom_canvas.width());
+      widgets['desc'] = new menu.TextBox(this.spob.text,
+         0,this.dom_canvas.height() - 18, 'desc',
+         () => {return this.dom_canvas.width()} );
+    }
+    return widgets;
   }
 };
--- src/menu.js
+++ src/menu.js
@@ -96,3 +96,71 @@ export class TextButton extends Widget{
     this.on_click();
   }
 }
+
+export class TextBox extends Widget{
+  /* TODO: For paragraphs: split by words:
+   * current_line = []
+   * if len(current_line) + 1 + word > line_len or
+   * if word == '\n':
+   * Start new line (repeat)
+   */
+  constructor(text, x, y, id, get_width_func){
+    super();
+    this.text = text;
+    this.x = x;
+    this.y = y;
+    this.id = id;
+    this.img = null;
+    this.text_size = 18;
+    this.text_width = Math.round(this.text_size * .888888888) // TODO: Figure out
+    this.get_width_func = get_width_func
+    console.log(this);
+  }
+
+  setup(group){
+    let line_number = 0
+    for (let line of line_break(
+          this.text, Math.round(this.get_width_func() / this.text_width))){
+      
+      new BABYLON.Text2D(line, {
+        id: this.id + line.toString(),
+        x: this.x,
+        y: this.y - ((this.text_size + 2) * line_number),
+        fontName: this.text_size.toString() + 'pt Courier',
+        parent: group
+      });
+
+      line_number += 1;
+    }
+  }
+} 
+
+export function line_break(text, max_line_width){
+
+  console.log(max_line_width);
+  console.log(text.length);
+  let words = text.split(" ");
+  let lines = [];
+  let line = "";
+  for (let word of words){
+    if( word === '\n'){
+      lines.push(line);
+      line = "";
+    } else {
+      if(1 + line.length + word.length <= max_line_width){
+        line += " " + word;
+      } else {
+        lines.push(line);
+        line = word;
+      }
+    }
+  }
+
+  if (line !== ""){
+    lines.push(line);
+  }
+  
+  console.log(lines);
+  return lines;
+}
+  
