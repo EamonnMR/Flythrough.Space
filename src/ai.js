2b25c96480349bc94b227413eb3d227e8c215fa3
EamonnMR <eamonn.m.r@gmail.com>
2017-08-15 10:09:00 -0400

    Revises AI code
    Removes a lot of duplication and variables from roids demo.

--- src/ai.js
+++ src/ai.js
@@ -1,3 +1,6 @@
+import * as collision from "collision";
+import * as physics from "physics";
+
 export function ai_system(entMan){
   for (let entity of entMan.get_with(['ai'])) {
     let ai = entity.ai;
@@ -9,75 +12,58 @@ export function ai_system(entMan){
   }
 };
 
-function engage (owner, deltaTime, target){
-		var cos;
-		var sin;
-		if(true){
-			var playerDistance = Math.sqrt( Math.pow(playership.x - this.x, 2) + Math.pow(playership.y - this.y, 2));
-			var playerAngle = Math.atan2(playership.y - this.y, playership.x - this.x) - this.spinState;
+function point_at(to, from){
+  let angle = Math.atan2(to.y - from.y, to.x - from.x);
+  
+  if(angle > arc){
+    playerAngle -= arc;
+  }
+  if(angle < -arc){
+    playerAngle += arc;
+  }
 
-			if(playerAngle > arc){
-				playerAngle -= arc;
-			}
-			if(playerAngle < -arc){
-				playerAngle += arc;
-			}
-			/*
-			roidsGame.context.fillStyle = 'white';
-			roidsGame.context.fillText(playerAngle, 10, 10);
-			
-			roidsGame.context.fillStyle = 'black';
-			*/
-			
-			if(playerAngle > .01){
-				this.spinState += this.spinSpeed * deltaTime;
-				if(this.spinState > arc){
-					this.spinState -= arc;
-				}
-			}
-			if(playerAngle < -.01){
-				this.spinState -= this.spinSpeed * deltaTime;
-				if(this.spinState < 0){
-					this.spinState += arc;
-				}
-			}
-			var dir = Math.atan2(this.vy, this.vx);
-			if(playerDistance > 50 &&  (playerAngle < .25 || playerAngle > -.25)){
-				//This is boilerplate acceleration code
-				this.vx += Math.cos( this.spinState) * deltaTime * this.accel;
-				this.vy += Math.sin( this.spinState) * deltaTime * this.accel;
-				dir = Math.atan2(this.vy, this.vx);
-				//Enforce max speed
-				
-				if(Math.sqrt( (this.vx * this.vx) + (this.vy * this.vy ) ) > this.maxSpeed){
-					//Find current heading vector
-					
-					
-					//Then set to max speed at that heading
-					this.vx = Math.cos(dir) * this.maxSpeed;
-					this.vy = Math.sin(dir) * this.maxSpeed;
-				}
-			}
-			if (this.shotTimer > 0){
-				this.shotTimer -= deltaTime;
-				if(this.shotTimer < 0){
-					this.shotTimer = 0;
-				}
-			} else {
-				if(playerDistance > 40 &&  (playerAngle < .2 || playerAngle > -.2)){
-					var cos = Math.cos(this.spinState);
-					var sin = Math.sin(this.spinState); 
-					//gameObjects.push(roidsGame.makeShot(this.x + (cos * 12), this.y + (sin * 12), 
-					this.vx +(cos * 100),
-					this.vy +(sin * 100),
-					this.spinState));
-					this.shotTimer = this.shotCoolDown;
-				}
-			}
-		}
-		
-		
-		this.x += this.vx * deltaTime;
-		this.y += this.vy * deltaTime;
-		
+  return angle;
+}
+
+function engage(entity, entMan){
+  let arc = Math.pi * 2;
+  let cos;
+  let sin;
+  
+  let target = entMan.get(entity.ai.target)
+
+  if(!target){
+    return  // Somehow inducate that we should change state
+  }
+
+  let distance = collision.distance(this.position, target.position);
+  
+  // Get the ideal facing
+  let goal_angle = physics.rotate(point_at(target.position, entity.position), -1 * entity.direction);
+  // If angle is outside a certain margin, rotate the ship to face the target
+  // Maybe pull this out into a "AI tries to face in a direction" system?
+  
+  angle = entity.rotation * entMan.delta_time // How far we _can_ turn this frame
+
+  if(goal_angle > .01){
+    physics.rotate(entity, angle);
+    entity.direction_delta = -1 * angle;
+  }
+  if(goal_angle < -.01){
+    physics.rotate(entity, -1 * angle);
+    entity.direction_delta = angle;
+  }
+
+  var dir = Math.atan2(this.vy, this.vx);
+  if(playerDistance > 50 &&  (playerAngle < .25 || playerAngle > -.25)){
+      physics.accelerate(entity.velocity, entity.direction,
+         entity.accel * entMan.delta_time); 
+  } else {
+    if(playerDistance > 40 &&  (playerAngle < .2 || playerAngle > -.2)){
+      for (let weapon of entity.weapons){
+        weapon.tryShoot(entMan, entity);
+      }
+    }
+  }
+  
 };
