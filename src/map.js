2b844b058dfed11f1bafa06ccb091466e06bdb8d
EamonnMR <eamonn.m.r@gmail.com>
2018-01-30 10:04:14 -0500

    Map progress - no longer crashes

--- src/map.js
+++ src/map.js
@@ -1,22 +1,39 @@
 import * as states from "states";
 import * as input from "input";
 
+// Z indices:
+
+const Z_SPACELANES = 1;
+const Z_SELECTED_SPACELANE = 2;
+const Z_SYSGROUP = 3;
+const Z_SYSCIRCLE = 4;
+const Z_SELECTION_DOT = 5;
+const Z_SYSTEXT = 6;
+const Z_OVERLAY = 7;
+
+const MAP_MAX_SIZE = "5000px";  // TODO: Calculate this from coordinates
+
+const TEXT_OFFSET = 10;
+const CIRCLE_SIZE = "9px";
+const CIRCLE_BACKGROUND = "Black";
+const CIRCLE_THICKNESS = "2px";
+
 export class MapView extends states.ViewState{
-  constructor(data, position, scene, game_canvas, player){
+  constructor(data, position, game_canvas, player){
     super();
 
     this.player = player;
     this.data = data;
     this.scale_factor = 1;
-    this.diff = {x: game_canvas.width() / 2, y: game_canvas.height() / 2};
+    //this.diff = {x: game_canvas.width() / 2, y: game_canvas.height() / 2};
 
-    this.offset = { x: position.x + this.diff.x,
+    /* this.offset = { x: position.x + this.diff.x,
                     y: position.y + this.diff.y };
-    this.scene = scene;
     this.game_canvas = game_canvas;
 
     this.canvas = null;
     this.map_image = null;
+    */
     this.selection = this.player.selected_system;
   }
   enter(){
@@ -28,21 +45,19 @@ export class MapView extends states.ViewState{
       },
       reset_game: () => {},
       hyper_jump: () => {}
-    }) 
-    this.canvas = new BABYLON.ScreenSpaceCanvas2D(this.scene, {
-      id: "map_canvas",
-      size: new BABYLON.Size(
-          this.game_canvas.width(), this.game_canvas.height()),
-      backgroundFill: "#4040408F"
-    });
+    })
+
+    this.adt = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("Map");
+
+    this.map_image = new BABYLON.GUI.Rectangle();
+    this.map_image.height = MAP_MAX_SIZE;
+    this.map_image.width = MAP_MAX_SIZE;
+
+    this.adt.addControl(this.map_image);
     
-    this.map_image = new BABYLON.Group2D({
-      parent: this.canvas,
-      id: 'map_image',
-      x: this.offset.x,
-      y: this.offset.y,
-    });
     let current = this.data.systems[this.selection];
+
+    /*
 		this.selection_img = new BABYLON.Ellipse2D({
       parent: this.map_image,
       id: 'selection',
@@ -52,9 +67,11 @@ export class MapView extends states.ViewState{
       height: 20,
       fill: BABYLON.Canvas2D.GetSolidColorBrushFromHex('#00FFFFFF') 
     });
-    
-    this.selection_img.zOrder = .9;
-
+    */
+   
+    /*
+     * TODO: This can be moved to the individual clickable
+     * system circles I think 
     this.map_image.pointerEventObservable.add(
       (d, s) => {
         let target = d.relatedTarget.id;
@@ -65,22 +82,20 @@ export class MapView extends states.ViewState{
       }, BABYLON.PrimitivePointerInfo.PointerUp
 
     );
+    */
 
     // Set up color fills for map drawing
     let govt_colors = {};
     let govt_dark_colors = {};
 
     for (let name of Object.keys(this.data.govts)){
-      govt_colors[ name ] = BABYLON.Canvas2D.GetSolidColorBrushFromHex(
-        this.data.govts[ name ].color
-      );
-      govt_dark_colors[ name ] = BABYLON.Canvas2D.GetSolidColorBrushFromHex(
-        this.data.govts[ name ].dark_color
-      );
+      govt_colors[ name ] = this.data.govts[ name ].color;
+      // TODO: Why aren't dark colors working?
+      govt_dark_colors[ name ] = this.data.govts[ name ].dark_color;
     }
 
-    let nogov_color = BABYLON.Canvas2D.GetSolidColorBrushFromHex('#AFAFAFFF');
-    let nogov_dark = BABYLON.Canvas2D.GetSolidColorBrushFromHex('#A9A9A9FF');
+    let nogov_color = '#AFAFAFFF';
+    let nogov_dark = '#A9A9A9FF';
 
     console.log(nogov_color);
     console.log(nogov_dark);
@@ -89,19 +104,25 @@ export class MapView extends states.ViewState{
 
     for ( let system of Object.keys(this.data.systems)) {
       let system_dat = this.data.systems[system];
-      let sys_text = new BABYLON.Text2D( system, {
-        parent: this.map_image,
-        id: system + '_label',
-        x: (circle_size + system_dat.x) * this.scale_factor,
-        y: (circle_size + system_dat.y) * this.scale_factor * -1,
-        fontName: '13pt Courier'
-      });
+      let system_block = new BABYLON.GUI.Rectangle();
+      system_block.height = "300px";
+      system_block.width = "300px";
+      system_block.alpha = 0;
+      system_block.zIndex = Z_SYSGROUP;
 
-      sys_text.zOrder = .7;
+      let sys_text = new BABYLON.GUI.TextBlock();
+      sys_text.color = "White";
+      sys_text.text = system_dat.name;
+      sys_text.alpha = 1;
+
+      system_block.addControl(sys_text);
+      sys_text.y_offset = TEXT_OFFSET;
+      sys_text.zIndex = Z_SYSTEXT;
       
-      let sys_loc_vec = new BABYLON.Vector2(system_dat.x * this.scale_factor,
-                                        -1 * system_dat.y * this.scale_factor)
-
+      //let sys_loc_vec = new BABYLON.Vector2(system_dat.x * this.scale_factor,
+      //                                  -1 * system_dat.y * this.scale_factor)
+      // TODO: This really should be made less redundant
+      /*
       for (let other_system_id of system_dat.links ){
         if (other_system_id in this.data.systems) {
           let other_system = this.data.systems[other_system_id];
@@ -125,6 +146,7 @@ export class MapView extends states.ViewState{
           console.log('bad link: ' + system + ' -> ' + other_system_id);
         }
       }
+      */
 
       // Determine the color of the circle based on government, if its empty
       let color = null;
@@ -135,24 +157,25 @@ export class MapView extends states.ViewState{
       } else {
         color = is_light ? nogov_color : nogov_dark;
       }
+      
+      let sys_circle = new BABYLON.GUI.Ellipse();
+      sys_circle.height = CIRCLE_SIZE;
+      sys_circle.width = CIRCLE_SIZE;
+      sys_circle.background = CIRCLE_BACKGROUND; 
+      sys_circle.color = color;
+      sys_circle.thickness = CIRCLE_THICKNESS;
+      sys_circle.zIndex = Z_SYSCIRCLE;
+      sys_circle.alpha = 1;
 
-      let sys_circle = new BABYLON.Ellipse2D({
-        parent: this.map_image,
-        id: system + '_circle',
-        x: (system_dat.x - circle_size / 2) * this.scale_factor,
-        y: (-1 * system_dat.y - (circle_size / 2)) * this.scale_factor,
-        width: circle_size,
-        height: circle_size,
-        fill: color
-      });
+      system_block.addControl(sys_circle);
 
-      sys_circle.zOrder = .5;
     }
     this.map_sub = null;
     this.draw_position();
     this.move = {x: 0, y: 0};
     this.dragging = false;
 
+    /*
     this.game_canvas.mousedown(() => {
       this.dragging = true;
     });
@@ -160,8 +183,9 @@ export class MapView extends states.ViewState{
     this.game_canvas.mouseup(() => {
       this.dragging = false;
     });
-
-    this.game_canvas.mousemove( (event) => {
+    */
+    //this.game_canvas.mousemove( (event) => {
+      /*
       if ( this.dragging ) {
         this.offset.x -= this.move.x - event.pageX;
         this.offset.y += this.move.y - event.pageY;
@@ -172,12 +196,14 @@ export class MapView extends states.ViewState{
       this.map_image.x = this.offset.x;
       this.map_image.y = this.offset.y;
       this.draw_position();
-    });
+      */
+    //});
 
   }
 
   exit(){
     input.unbindInputFunctions();
+    /*
     this.game_canvas.unbind('mousedown');
     this.game_canvas.unbind('mouseup');
     this.game_canvas.unbind('mousemove');
@@ -186,10 +212,12 @@ export class MapView extends states.ViewState{
       x: this.offset.x - this.diff.x,
       y: this.offset.y - this.diff.y,
     };
+    */
     this.player.selected_system = this.selection
   }
 
   move_selection( system_name ){
+    /*
     // TODO: We could probably replace some of the enter code
     // with a call to this
     this.selection = system_name;
@@ -197,12 +225,15 @@ export class MapView extends states.ViewState{
     let sel_system = this.data.systems[system_name];
     this.selection_img.x = (sel_system.x - 10) * this.scale_factor;
     this.selection_img.y = (-1 * sel_system.y - 10) *  this.scale_factor;
+    */
   }
 
   draw_position(){
+    /*
     if (this.map_sub){
       this.map_sub.dispose();
     }
+    */
     /*
     this.map_sub = new BABYLON.Text2D('' + this.offset.x + ',' + this.offset.y,
       {
