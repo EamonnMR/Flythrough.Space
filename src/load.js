bfc20619e2c6ff10bdbd5d8dc5263b62fb69ff42
EamonnMR <Eamonn.m.r@gmail.com>
2020-04-01 20:57:32 -0400

    Scaling...
    Applying the scale does not change the fact that when you move objects they move in sized up space.

--- src/load.js
+++ src/load.js
@@ -339,6 +339,8 @@ function load_assets( source_json, scene, data, finish_callback ){
 
       meta_blob.attachpoint_map = get_attach_points_from_model_load_task(task);
       hide_all_meshes_for_task(task);
+      // Scaling does not work, why?
+      /*
       if("scale" in meta_blob){
         let mesh = meta_blob.mesh;
         mesh.scaling.x = meta_blob.scale;
@@ -349,6 +351,7 @@ function load_assets( source_json, scene, data, finish_callback ){
           mesh.getWorldMatrix()
         );
       }
+      */
     }
 
     // TODO: Do we need to get the offsets from ._children
